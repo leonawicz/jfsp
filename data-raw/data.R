2dc6e54dfa74b73d9402f5afbd78f2a12f957d1e
leonawicz <matt.rstat@gmail.com>
2018-03-06 15:34:54 -0700

    Update paths, add attributes for data summary documents.

--- data-raw/data.R
+++ data-raw/data.R
@@ -29,7 +29,7 @@ fbxfire <- readRDS("C:/github/jfsp-archive/data-raw/fire_prob_fbks_simplified.rd
   mutate(RCP = factor(sapply(as.character(RCP), f), levels = lev))
 
 gcms <- c("CCSM4", "GFDL-CM3", "GISS-E2-R", "IPSL-CM5A-LR", "MRI-CGCM3")
-fmoba <- readRDS("C:/github/jfsp-archive/data-raw/ba_fmo/ba_fmo2_alaska.rds")
+fmoba <- readRDS("C:/github/jfsp-archive/data-raw/ba_fmo/ba_fmo2_sw_alaska.rds")
 sets <- strsplit(fmoba$Set, "\\.")
 fmoba <- mutate(fmoba, Set = sapply(sets, "[", 1), RCP = sapply(sets, "[", 2),
                 Model = gsub("CRU32", "CRU 3.2", sapply(sets, "[", 3))) %>%
@@ -40,7 +40,7 @@ fmoba <- mutate(fmoba, Set = sapply(sets, "[", 1), RCP = sapply(sets, "[", 2),
                                    ifelse(Tx == "tx1", "Treatment 1",
                                           ifelse(Tx == "tx2", "Treatment 2",
                                                  ifelse(Tx == "none", "No management", Tx)))))))
-fmoba2 <- mutate(readRDS("C:/github/jfsp-archive/data-raw/ba_fmo/ba_fmo_alaska.rds"),
+fmoba2 <- mutate(readRDS("C:/github/jfsp-archive/data-raw/ba_fmo/ba_fmo_sw_alaska.rds"),
              Set = substr(Set, 1, 9), RCP = factor("Historical", levels = lev),
              Model = "Observed", Tx = "Status quo")
 fmoba <- bind_rows(fmoba, fmoba2) %>%
@@ -77,6 +77,59 @@ costSummary <- mutate(mgmtcost, Decade = Year - Year %% 10) %>%
   bind_cols(as_data_frame(x)) %>% filter(Year >= 2020) %>% tidyr::gather("cost", "value", 13:1012) %>%
   group_by(Set, Tx, RCP, Decade) %>%
   summarise(`5th percentile` = quantile(value, prob = 0.05),
-            Mean = mean(value), `95th percentile` = quantile(value, prob = 0.95)) %>% ungroup
+            Mean = mean(value), `95th percentile` = quantile(value, prob = 0.95)) %>% ungroup %>%
+  select(-Set)
+
+# set variable labels and attributes
+set_atts <- function(data, lab, desc){
+  x <- names(data)
+  for(i in 1:ncol(data)){
+    if(!is.na(lab[i])) attr(data[[x[i]]], "labels") <- lab[i]
+    if(!is.na(desc[i])) attr(data[[x[i]]], "shortDescription") <- desc[i]
+  }
+  data
+}
+
+set <- c("Observed/ALFRESCO run", "Historical observations or ALFRESCO output based on various run parameterizations")
+tx <- c("Control/Treatment run", "The control group is 'status quo' fire management and other values are different fire management treatments")
+rcp <- c("Representative concentration pathways", "RCPs based on different greenhouse gas emissions scenarios")
+fmo <- c("Fire management options", "Fire management options refer to regions of Alaska where a particular management strategy is applied")
+yr <- c("Year", "")
+dec <- c("Decade", "")
+ba <- c("Burn area", "The annual area burned")
+cba <- c("Cumulative burn area", "The cumulative annual area burned")
+basd <- c("10-yr MA burn area SD", "The inter-annual variability in area burned given by the 10-year moving average of the standard deviation in burn area")
+
+lab <- c(set[1], tx[1], rcp[1], fmo[1], yr[1], ba[1], cba[1], basd[1])
+desc <- c(set[2], tx[2], rcp[2], fmo[2], yr[2], ba[2], cba[2], basd[2])
+fmoba <- set_atts(fmoba, lab, desc)
+
+lab <- c(set[1], tx[1], rcp[1], fmo[1], "Minimum", "First quartile", "Median", "Mean", "Third quartile", "Maximum")
+desc <- c(set[2], tx[2], rcp[2], fmo[2], rep("Summary statistic: all years", 6))
+fmobaSummary <- set_atts(fmobaSummary, lab, desc)
+
+lab <- c(tx[1], rcp[1], yr[1], "Management cost statistic", "Management cost")
+desc <- c(tx[2], rcp[2], yr[2], "5th percentile, mean, and 95th percentile statistics for the distribution of MC cost samples", "Fire management cost in millions of dollars")
+cost <- set_atts(cost, lab, desc)
+
+lab <- c(tx[1], rcp[1], dec[1], "Lower bound cost", "Mean cost", "Upper bound cost")
+desc <- c(tx[2], rcp[2], dec[2], "5th percentile fire management cost in millions of dollars", "Mean fire management cost in millions of dollars", "95th percentile fire management cost in millions of dollars")
+costSummary <- set_atts(costSummary, lab, desc)
+
+lab <- c(tx[1], rcp[1], "Time period", "Spatial buffer", "P(Fire)")
+desc <- c(tx[2], rcp[2], "Aggregate time period in years", "Spatial buffer radius in km", "The probability of fire near Fairbanks, Alaska in ALFRESCO output")
+fbxfire <- set_atts(fbxfire, lab, desc)
+
+lab <- c(tx[1], rcp[1], yr[1], "Coniferous:Deciduous ratio")
+desc <- c(tx[2], rcp[2], yr[2], "The ratio of coniferous to deciduous species on the Alaska landscape through time in ALFRESCO output")
+cdratio <- set_atts(cdratio, lab, desc)
+
+lab <- c(tx[1], rcp[1], dec[1], "Fire size", "Frequency")
+desc <- c(tx[2], rcp[2], dec[2], "Fir size distributions per year in acres with a minimum of resolution of 247 acres after conversion to acres from native 1-km resolution ALFRESCO output", "Frequency of fires of a given size per year in ALFRESCO output")
+firesize <- set_atts(firesize, lab, desc)
+
+lab <- c("Limited management", "Modified management", "Critical management", "Full management")
+desc <- c("Historical annual empirical fire management costs per unit area (dollars/acre) for the years 2001 - 2017")
+basecost <- set_atts(select(basecost, c(1, 2, 4, 3)), lab, desc)
 
 usethis::use_data(basecost, cdratio, fbxfire, fmoba, fmobaSummary, cost, costSummary, firesize)
