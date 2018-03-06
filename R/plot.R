948faf56d05943c6052879dd632f783fbae2d71c
leonawicz <matt.rstat@gmail.com>
2018-03-06 16:11:02 -0700

    Add namespace.

--- R/plot.R
+++ R/plot.R
@@ -37,7 +37,7 @@
       dplyr::summarise(value = mean(.data[["value"]])) %>% dplyr:: ungroup()
   if(type == "cost_dec"){
     x <- dplyr::ungroup(x)
-    if("FMO" %in% names(x)) x <- group_by("FMO")
+    if("FMO" %in% names(x)) x <- dplyr::group_by("FMO")
     dplyr::group_by(x, .data[["Set"]], .data[["Tx"]], .data[["Decade"]], add = TRUE) %>%
       dplyr::summarise(`5th percentile` = mean(.data[["5th percentile"]]),
                        Mean = mean(.data[["Mean"]]),
